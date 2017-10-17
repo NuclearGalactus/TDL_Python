c00287feadeca2e37284f45094e8345aec155b0a
Unknown <thomascareythegalactictsar@outlook.com>
2017-10-17 15:18:43 -0400

    Added main python file

---
+++ tdl.py
@@ -0,0 +1,42 @@
+import numpy as np;
+import sys;
+LENGTH = 8;
+INTERVAL = 0.5;
+NUM = int(LENGTH / INTERVAL);
+REWARDT = 12;
+
+
+values = np.zeros((NUM));
+perr = np.zeros((NUM));
+weights = np.zeros((NUM));
+rewards = np.zeros((NUM));
+cues = np.zeros(NUM);
+cues[8] = 1;
+for i in range(REWARDT,NUM):
+    rewards[i] = 1;
+
+def trial():
+    for i in range(0,NUM-1):
+        perr[i] = (values[i+1] - values[i] + rewards[i]);
+        
+        		
+    print("Values:  ",end='');
+    print(values);
+    print("PredErr: ",end='');
+    print(perr);
+    print("Weights: ",end='');
+    print(weights);
+    print("Rewards: ",end='');
+    print(rewards);
+
+def run():
+    currVal = "";
+    while(currVal != "done"):
+        currVal = input("Awaiting Input:").lower();
+        print(currVal);
+        if(currVal == "run"):
+             print("Ran 1 trial!");
+             trial();
+run();
+
+	
